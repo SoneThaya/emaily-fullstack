50a39cbd222e8132c9ca21a92cdb50bfd8d3357c
Sone <sthaya@gmail.com>
2020-12-25 21:17:25 -0800

    setup redux in index file

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './components/App'
+import React from "react";
+import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-ReactDom.render(<App />, document.querySelector("#root"));
+import App from "./components/App";
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
