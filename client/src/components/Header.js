7814fc8561b5a9702f346c25758e85a2570da72f
Sone <sthaya@gmail.com>
2020-12-27 18:36:04 -0800

    set up header and created landing component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -26,7 +26,7 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <Link to="/" className="brand-logo">
+          <Link to={this.props.auth ? "/surveys" : "/"} className="brand-logo">
             Emaily
           </Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
