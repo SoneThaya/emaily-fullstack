c96240567b8bfb57b7ae4dee211dfa4eba7871f2
Sone <sthaya@gmail.com>
2021-01-05 20:19:33 -0800

    installed grid send dependancy

---
+++ sendgrid-test/test.js
@@ -0,0 +1,12 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(SENDGRID_API_KEY);
+const msg = {
+  to: "sthaya@gmail.com",
+  from: "sthaya@gmail.com",
+  subject: "Sending with Twilio SendGrid is Fun",
+  text: "and easy to do anywhere, even with Node.js",
+  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+};
+sgMail.send(msg);
