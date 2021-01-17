09669dbd67c9988224cd498cdef9b26eac52df6c
Sone <sthaya@gmail.com>
2021-01-16 19:08:56 -0800

    worked on mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,7 +2,14 @@ const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
 
 class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
 
+    this.from_email = helper.Email("no-reply@emaily.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+  }
 }
 
 module.exports = Mailer;
