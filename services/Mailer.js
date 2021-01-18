5bd333ce0513267a8a133d48e0aa2a5b4f9d4f10
Sone <sthaya@gmail.com>
2021-01-17 17:46:05 -0800

    setup sending emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -5,6 +5,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(process.env.SENDGRID_API_KEY);
     this.from_email = helper.Email("no-reply@emaily.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
@@ -27,6 +28,27 @@ class Mailer extends helper.Mail {
 
     trackingSettings.setClickTracking(clickTracking);
     this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach((recipient) => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: this.toJSON(),
+    });
+
+    const response = this.sgApi.API(request);
+
+    return response;
   }
 }
 
