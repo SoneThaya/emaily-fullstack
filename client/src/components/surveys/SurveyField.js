299d7ab1d4170dc78c0b894d707b03ff9ee03b02
Sone <sthaya@gmail.com>
2021-01-18 19:55:57 -0800

    setup validation to appear for input fields

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { getFormMeta } from "redux-form";
 
 // surveyField contains logic to render a single label and text input
-export default ({ input, label }) => {
-  console.log(input);
+export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+      {touched && error}
     </div>
   );
 };
