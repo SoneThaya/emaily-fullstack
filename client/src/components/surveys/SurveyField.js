ce6b247849b4f43de52491bb74ef32577bc38a66
Sone <sthaya@gmail.com>
2021-01-18 19:29:46 -0800

    refactored input field code

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 // surveyField contains logic to render a single label and text input
-export default () => {
-    return (
-        <div>
-            <input />
-        </div>
-    )
-}
+export default ({ input, label }) => {
+  console.log(input);
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
