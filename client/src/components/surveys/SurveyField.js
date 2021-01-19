5f2c020c055563f8774d55e74dd88f8186577a93
Sone <sthaya@gmail.com>
2021-01-18 21:22:00 -0800

    refactored email validation

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { getFormMeta } from "redux-form";
 
 // surveyField contains logic to render a single label and text input
 export default ({ input, label, meta: { error, touched } }) => {
