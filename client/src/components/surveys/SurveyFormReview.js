3bd59ed8d98ea0184448e21696d05602bb6a1dd9
Sone <sthaya@gmail.com>
2021-01-19 18:57:46 -0800

    set up back button on survey from review to go back

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const SurveyFormReview = () => {
-    return (
-        <div>
-            <h5>Please confirm your entries</h5>
-        </div>
-    )
-}
+// survey form review shows users their form inputs for review
+const SurveyFormReview = ({ onCancel }) => {
+  return (
+    <div>
+      <h5>Please confirm your entries</h5>
+      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+        Back
+      </button>
+    </div>
+  );
+};
 
-export default SurveyFormReview
+export default SurveyFormReview;
